620df29839d69871cc7673d822ee77e84b9ba600
GiuliaR11 <radugiulia@yahoo.com>
2020-12-02 20:46:18 +0200

    [FE0028] Refactored sidemenu using Evergreen

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
-import {ArticlePage} from './article-page/ArticlePage';
 import {SideMenu} from'./sidemenu/SideMenu';
 
 function App() {
   return (
     <div>
-      {/* <ArticlePage/>       */}
       <SideMenu/>
     </div>
   );
 }
-
 export {App};
