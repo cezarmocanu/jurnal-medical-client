66df85567386b979f0812610f6a927fa21c7c55b
Andreias Bogdan <bogdanandreias1923@gmail.com>
2020-12-10 14:13:08 +0200

    [FE0071] Sidemenu fixed in the left side of the page while scrolling

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -3,24 +3,24 @@ import { Pane, Text } from 'evergreen-ui'
 import { Screen } from '../screen/Screen'
 
 function HomePage() {
-    return(
-    <Screen title="Home Page">
-        <Pane
-            elevation={4}
-            float="left"
-            width={200}
-            height={120}
-            margin={24}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            flexDirection="column"
-        >
-          <Text>Cezar</Text>
-          <Text size={300}>Cel Mare</Text>
-        </Pane>
-    </Screen>
+    return (
+        <Screen title="Home Page">
+            <Pane
+                elevation={4}
+                float="left"
+                width={200}
+                height={120}
+                margin={24}
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                flexDirection="column"
+            >
+                <Text>Cezar</Text>
+                <Text size={300}>Cel Mare</Text>
+            </Pane>
+        </Screen>
     )
 }
 
-export {HomePage};
+export { HomePage };
