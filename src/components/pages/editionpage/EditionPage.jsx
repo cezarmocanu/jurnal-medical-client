e5c94e203ed88ca667c1408aaddb07e95846fa8a
Cezar Mocanu <cmocanu@Cezars-MacBook-Pro.local>
2020-12-22 13:55:43 +0200

    [FE0111] Added lorem generator and Edition Card

--- src/components/pages/editionpage/EditionPage.jsx
+++ src/components/pages/editionpage/EditionPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Page} from '../../page/Page'
 import { LABELS } from '../../../strings';
+import { EditionCard } from '../../cards/edition-card/EditionCard';
 
+import './EditionPage.scss';
 
 function EditionPage(){
   return (
-    <Page title={LABELS.editions}>
-      
+    <Page title={LABELS.editions} className="edition-page">
+      <EditionCard title={LABELS.loremTitle(2)} id={5}/>
+      <EditionCard title={LABELS.loremTitle(2)} id={5}/>
+      <EditionCard title={LABELS.loremTitle(2)} id={5}/>
+      <EditionCard title={LABELS.loremTitle(2)} id={5}/>
     </Page>
   );
 }
